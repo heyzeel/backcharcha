6e8d1e3f94d0c3098a24f961da7f8001b48c1666
zeelelelel <allinoneshoppingaffiliate@gmail.com>
2024-09-07 21:29:17 +0530

    final3

--- index.js
+++ index.js
@@ -10,13 +10,19 @@ const cors = require('cors');
 connectDB();
 const app = express();
 app.use(express.json());
-app.use((req, res, next) => {
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "https://main--0charcha.netlify.app"
-    );
-})
-app.use(cors());
+// app.use((req, res, next) => {
+//     res.setHeader(
+//       "Access-Control-Allow-Origin",
+//       "https://main--0charcha.netlify.app"
+//     );
+// })
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ 
+app.use(cors(corsOptions))
 
 
 app.use("/api/user", userRoutes);
