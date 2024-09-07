5ce049e48e3a09c1877e75cbacd90f414992b93b
zeelelelel <allinoneshoppingaffiliate@gmail.com>
2024-09-07 21:56:08 +0530

    final4

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const server = app.listen(PORT, (req, res) => {
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: 'https://0charcha.netlify.app'
+        origin: '*'
     }
 })
 
